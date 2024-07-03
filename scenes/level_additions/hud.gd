6f44f74ba5f17806219f7e736188df26992f8507
NemericTiger <Paul.l.messerschmidt@gmail.com>
2024-07-03 22:09:39 +0200

    dias placed

--- scenes/level_additions/hud.gd
+++ scenes/level_additions/hud.gd
@@ -6,13 +6,13 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	$Currency_Counter.text = "Diamonds: " + str(GlobalVar.currency_01)
+	$Currency_Counter.text = "Diamonds: " + str(GlobalVar.currency_01) + "/20"
 	print(GlobalVar.alive)
 	#if GlobalVar.alive == false:
 		#new_modulate.a = 255
 
 func _on_dia_body_entered(_body):
-	$Currency_Counter.text = "Diamonds: " + str(GlobalVar.currency_01)
+	$Currency_Counter.text = "Diamonds: " + str(GlobalVar.currency_01) + "/20"
 
 
 func _physics_process(_delta):
